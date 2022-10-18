04e894c093aad1b5efe957dd04108771d82aedb8
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-10-18 17:59:19 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,162 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	//пример
+	/*int i = 1;
+	while (i <= 5) {
+		cout << i << " ";
+		i++;
+	}*/
+
+	//1
+	/*int i = 7;
+	while (i <= 98) {
+		cout << i << " ";
+		i += 7;
+	}*/
+
+	//2
+	/*int i = 1;
+	while (i <= 512) {
+		cout << i << " ";
+		i = i * 2;
+	}*/
+
+	//задачи
+
+	//8
+	/*int i = 20;
+	while (i <= 50) {
+		if ((i % 3) == 0 && 0 != (i % 5))
+		{
+			cout << i << endl;
+		}
+		i++;
+	}*/
+
+	//9
+	/*int i = 35;
+	while (i <= 87) {
+		if ((i % 7) == 1 || (i % 7) == 2 || (i % 7) == 5 )
+		{
+			cout << i << endl;
+		}
+		i++;
+	}*/
+
+	//10
+	/*int i = 1;
+	int s = 0;
+	while (i <= 50) {
+		if ((i % 7) == 0 || (i % 5) == 0)
+		{
+			s = i + s;
+			
+		}
+		i++;
+	}
+	cout << s << endl;*/
+
+	//11
+	/*int i = 10;
+	while (i <= 99) {
+		if ((i % 4) == 0 && 0 != (i % 6))
+		{
+			cout << i << endl;
+		}
+		i++;
+	}*/
+
+	//12
+	/*int a = 1;
+	int i = 10;
+	while (i <= 99) {
+		if ((0 != (i % 2) && (i % 13) == 0))
+		{
+			a = i * a;
+		}
+		i++;
+	}
+	cout << a << endl;*/
+
+	//13
+	/*int a = 0;
+	int i = 100;
+	while ( i <= 200) {
+		if ((i % 17) == 0) {
+			a = i + a;
+		}
+		i++;
+	}
+	cout << a << endl;*/
+
+	//14
+	/*double a = 0;
+	int n;
+	int i = 1;
+	cin >> n;
+	while (i <= n) {
+
+		a = (i * i) + a;
+		i++
+
+	}
+	cout << a << endl;*/
+
+	//17
+//double n;
+//double SUMr = 0;
+//cin >> n;
+//int i = 1;
+//while ( i <= n) {
+//	double r;
+//	cin >> r;
+//	SUMr = SUMr + r;
+//	i++;
+//}
+//double MIDr = SUMr / n;
+//cout << MIDr << endl;
+
+//18
+//int i = 1;
+//int n;
+//cin >> n;
+//int k = 0;
+//	while (i <= n) {
+//		if ((i % 2) != 0 && (i % 3) != 0 && (i % 5) != 0)
+//		{
+//			k = k + 1;
+//		}
+//		i++;
+//	}
+//	cout << k;
+
+//29
+//int i = 2;
+//while (i <= 100) {
+//	cout << i << " ";
+//	i += 2;
+//}
+
+//30
+//int i = 1;
+//while (i <= 99) {
+//	cout << i << " ";
+//	i += 2;
+//}
+
+
+
+
+	
+
+
+
+
+
+
+
+}
